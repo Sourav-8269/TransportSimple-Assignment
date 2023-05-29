1de436aa347f038315be4b350f0d9d6bccd15272
Sourav <souravsahu70047@gmail.com>
2023-05-29 20:22:00 +0530

    Added Home Page with some sections

--- ts-app/src/Components/Home.jsx
+++ ts-app/src/Components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../Styles/home.css"
+import mechanicalengineer from "../Images/mechanical-engineer.jpg";
 const Home = () => {
   return (
     <div>
       <div id="section_1">
         <div>
-          <h2>Content fleet management <br/>Content fleet</h2>
+          <h1>Content fleet management <br/>Content fleet</h1>
           <p>
             TransportSimple has been built with a vision to revolutionise the
             logistics industry globally. It is the perfect one-stop fleet
@@ -19,6 +20,27 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <section id="section_2">
+        <div id="section_2_heading">
+          <div>
+            <p>
+              KNOW ABOUT <span style={{color:"#1da1f2",fontWeight:"600"}} >TRANSPORTSIMPLE</span>
+            </p>
+            <h1 style={{marginTop:"-15px"}} >At our company, we specialize in providing comprehensive</h1>
+          </div>
+          <p>
+            TransportSimple has been built with a vision to revolutionise the
+            logistics industry globally. It is the perfect one-stop fleet
+            management solution suitable for all fleet sizes. TransportSimple
+            has been built with a vision to revolutionise the logistics industry
+            globally. It is the perfect one-stop fleet management solution
+            suitable for all fleet sizes.
+          </p>
+        </div>
+        <div id="engineer_div">
+            <img id="enginner_img" src={mechanicalengineer} alt="Engineer" srcset="" />
+        </div>
+        </section>
     </div>
   );
 };
