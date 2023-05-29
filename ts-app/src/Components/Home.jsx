341a90aef2e9ca9111c19f63760d67a5543286ed
Sourav <souravsahu70047@gmail.com>
2023-05-29 23:38:58 +0530

    Added Files

--- ts-app/src/Components/Home.jsx
+++ ts-app/src/Components/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import "../Styles/home.css"
+import "../Styles/home.css";
 import mechanicalengineer from "../Images/mechanical-engineer.jpg";
-import {FiTruck} from "react-icons/fi"
+import { FiTruck } from "react-icons/fi";
 const Home = () => {
   return (
     <div>
       <div id="section_1">
         <div>
-          <h1>Content fleet management <br/>Content fleet</h1>
+          <h1>
+            Content fleet management <br />
+            Content fleet
+          </h1>
           <p>
             TransportSimple has been built with a vision to revolutionise the
             logistics industry globally. It is the perfect one-stop fleet
@@ -25,9 +28,14 @@ const Home = () => {
         <div id="section_2_heading">
           <div>
             <p>
-              KNOW ABOUT <span style={{color:"#1da1f2",fontWeight:"600"}} >TRANSPORTSIMPLE</span>
+              KNOW ABOUT{" "}
+              <span style={{ color: "#1da1f2", fontWeight: "600" }}>
+                TRANSPORTSIMPLE
+              </span>
             </p>
-            <h1 style={{marginTop:"-15px"}} >At our company, we specialize in providing comprehensive</h1>
+            <h1 style={{ marginTop: "-15px" }}>
+              At our company, we specialize in providing comprehensive
+            </h1>
           </div>
           <p>
             TransportSimple has been built with a vision to revolutionise the
@@ -39,22 +47,33 @@ const Home = () => {
           </p>
         </div>
         <div id="engineer_div">
-            <img id="engineer_img" src={mechanicalengineer} alt="Engineer"/>
-            <div>
+          <img id="engineer_img" src={mechanicalengineer} alt="Engineer" />
+          <div id="vehicle_catalog" >
             {/* <FiTruck size="20" /> */}
-              <div>
-                  {/* <div> */}
-                    <FiTruck color="#4AA0FC" size={"20px"} />
-                  {/* </div> */}
-                  <p>135400</p>
-                  <p>vehicles</p>
-              </div>
+            <div>
+              <FiTruck color="#4AA0FC" size={"20px"} />
+              <p>135400</p>
+              <p>vehicles</p>
             </div>
+            <div>
+              <FiTruck color="#4AA0FC" size={"20px"} />
+              <p>135400</p>
+              <p>vehicles</p>
+            </div>
+            <div>
+              <FiTruck color="#4AA0FC" size={"20px"} />
+              <p>135400</p>
+              <p>vehicles</p>
+            </div>
+            <div>
+              <FiTruck color="#4AA0FC" size={"20px"} />
+              <p>135400</p>
+              <p>vehicles</p>
+            </div>
+          </div>
         </div>
-        </div>
-        <div id="section_3">
-
-        </div>
+      </div>
+      <div id="section_3"></div>
     </div>
   );
 };
